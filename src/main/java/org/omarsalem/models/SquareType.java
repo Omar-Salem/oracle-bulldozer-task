fbf72da254e9b7c1135532069ee3d36c827bcfc1
Omar Salem <omar.salem@etika.com>
2024-10-27 13:39:06 +0400

    refactored SquareType.java

--- src/main/java/org/omarsalem/models/SquareType.java
+++ src/main/java/org/omarsalem/models/SquareType.java
@@ -1,16 +1,16 @@
 package org.omarsalem.models;
 
 public enum SquareType {
-    PLAIN(1, 'o'),
-    CLEARED(1, 'c'),
-    ROCKY(2, 'r'),
-    TREE(2, 't'),
-    PROTECTED_TREE(2, 'T');
+    PLAIN('o', 1),
+    CLEARED('c', PLAIN.fuelCost),
+    ROCKY('r', PLAIN.fuelCost * 2),
+    TREE('t', PLAIN.fuelCost * 2),
+    PROTECTED_TREE('T', PLAIN.fuelCost * 2);
 
     private final int fuelCost;
     private final char symbol;
 
-    SquareType(int fuelCost, char symbol) {
+    SquareType(char symbol, int fuelCost) {
         this.fuelCost = fuelCost;
         this.symbol = symbol;
     }
